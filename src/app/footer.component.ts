9d7d3be125df184fbc1cbdcd7627806479fad244
Jean <jean.sordes@yahoo.fr>
2019-07-15 08:57:53 +0200

    attempt to fix assets URL

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     template: `
 <footer>
     <p>Angular web app made by <a href="https://www.jzs.fr">Jean Z. SORDES</a>
-    for a test <a href="./assets/exercice.pdf">(see the requirements here)</a> for
+    for a test <a href="assets/exercice.pdf">(see the requirements here)</a> for
     <a href="https://www.cgx-group.com">CGX Group</a>, made in less than a weekend, in July 2019</p>
 </footer>
 `
